2a0173d436fa5198cb7ee92dd0b7e97431761f58
kangkang <2646381627@qq.com>
2022-08-24 22:57:59 +0800

    now the kicks can be played correctly (sync ui)

--- Main.gd
+++ Main.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 var play = false
 var kick_paizi = [0,0,0,0 ,0,0,0,0 ,0,0,0,0 ,0,0,0,0] # 16拍一个小节
+var cur_kick = 0
 
 func _ready():
 	var bpm = 130
-	var per_pai = 0.46 # 一拍多少秒
+	var per_pai = 0.146 # 一拍多少秒
 	#print_debug(per_pai)
 	print_debug(kick_paizi)
+	
+	$Timer.wait_time = per_pai
 	
 	for node in $JiJIa/kick.get_children():
 		node.connect("toggled", self, "_on_kick_toggled", [node.get_index()])
@@ -25,26 +28,41 @@ func _on_kick_toggled(butto_pressed: bool, button_index: int):
 func _on_Play_pressed():
 	play = true
 
-	for i in kick_paizi:
-		if i == 1:
-			$Timer.start()
+	#for i in kick_paizi:
+		#if i == 1:
+		#$Timer.start()
+		
+	$Timer.start() # 末尾再来一个
 	
 	print_debug(kick_paizi)
 	pass
 
 func _on_Stop_pressed():
 	play = false
+	cur_kick = 0
 	
-	for i in kick_paizi:
-		if i == 1:
-			$Timer.stop()
+	#for i in kick_paizi:
+		#if i == 1:
+		#$Timer.stop()
+		
+	$Timer.stop()
 			
 	print_debug(kick_paizi, "stoped.")
 	pass 
 
 func _on_Timer_timeout():
+	var next_kick = 0
+	if (cur_kick + 1) > 16:
+		cur_kick = next_kick
+		return
+	else:
+		next_kick = cur_kick + 1
 	
-	$Kick.play()
+	print_debug(cur_kick)
+	if kick_paizi[cur_kick] == 1:	
+		$Kick.play()
+	
+	cur_kick = next_kick
 	
 	pass 
 
