48b8b752399d6ae696aca8849803e87c16ddeb94
2439905184 <suchenglong018@126.com>
2022-08-25 09:49:12 +0800

    🎈 perf: 优化动态内存显示

--- MemoryUsedDisplay.gd
+++ MemoryUsedDisplay.gd
@@ -2,5 +2,5 @@ extends Label
 
 func _process(delta):
 	var memory = OS.get_dynamic_memory_usage()
-	self.text = "Memory:" + str(memory)
+	self.text = "Memory:" + str(memory/1000/1000) + "MB"
 	pass
